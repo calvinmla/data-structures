ee8cae3a4ad97da021d85ef9231d744b8381f147
Calvin <calvinminhla@gmail.com>
2020-10-27 15:11:49 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,16 +5,16 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     var newNode = new Node(value);
-    
+
     if (this.tail) { //while the tail is pointing to something / has something
-      
+
       this.tail.next = newNode; // make the next pointer point to the  new node so the link is established
-    } else { 
-      //start here if linklist is empty 
+    } else {
+      //start here if linklist is empty
       // console.log('1st : ');
-      this.head = newNode; // 1st. makes the head the new node 
+      this.head = newNode; // 1st. makes the head the new node
     }
- 
+
     this.tail = newNode; // 2nd. make also the tale point to the very first node because only one node exist
     // 3rd. if you add more to the existing list then you go into the if statement.
   };
@@ -26,18 +26,15 @@ var LinkedList = function() {
   };
 
   list.contains = function(target) {
-
     var currentNode = this.head;
-
-    while (currentNode) {  
+    while (currentNode) {
       if (currentNode.value === target) {
         return true;
-      } 
+      }
       currentNode = currentNode.next;
     }
     return false;
   };
-
   return list;
 };
 
@@ -52,4 +49,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+addToTail = O(1) constant time;
+removeHead = O(1) constant time;
+contains = O(n) linear;
  */
